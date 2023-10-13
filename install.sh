9820d7c4f13818ed5f3bba4aa537fe0660af033a
Becky <bckyjns@gmail.com>
2023-10-13 18:47:50 +0100

    ♻️ move symlinks higher up

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ set -e
 DOTFILES_LOCATION=$(pwd)
 export DOTFILES_LOCATION;
 
+echo "🔗 Creating symlinks"
+stow starship zsh
+
 ./bin/dotfiles install oh-my-zsh
 ./bin/dotfiles install brew
 
-stow starship zsh
 
 echo "👷‍♀️ Manual installation/updates"
 echo "oh-my-zsh      omz update"
