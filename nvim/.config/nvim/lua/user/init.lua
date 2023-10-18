c971f886cd0c3afa21082b2861d103808ea2b5fe
Becky <bckyjns@gmail.com>
2023-10-18 10:42:04 +0100

    ✨ set all the volar options, all of them

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -31,16 +31,16 @@ end
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin",     -- remote to use
-    channel = "stable",    -- "stable" or "nightly"
-    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly",    -- branch name (NIGHTLY ONLY)
-    commit = nil,          -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false,  -- skip prompts about breaking changes
+    remote = "origin", -- remote to use
+    channel = "stable", -- "stable" or "nightly"
+    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly", -- branch name (NIGHTLY ONLY)
+    commit = nil, -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false,     -- automatically quit the current session after a successful update
-    remotes = {            -- easily add new remotes to track
+    auto_quit = false, -- automatically quit the current session after a successful update
+    remotes = { -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -61,7 +61,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
+        enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -123,6 +123,45 @@ return {
       },
       volar = {
         filetypes = { "vue", "typescript", "javascript" },
+        init_options = {
+          languageFeatures = {
+            implementation = true, -- new in @volar/vue-language-server v0.33
+            references = true,
+            definition = true,
+            typeDefinition = true,
+            callHierarchy = true,
+            hover = true,
+            rename = true,
+            renameFileRefactoring = true,
+            signatureHelp = true,
+            codeAction = true,
+            workspaceSymbol = true,
+            completion = {
+              defaultTagNameCase = "both",
+              defaultAttrNameCase = "kebabCase",
+              getDocumentNameCasesRequest = false,
+              getDocumentSelectionRequest = false,
+            },
+            documentHighlight = true,
+            documentLink = true,
+            codeLens = { showReferencesNotification = true },
+            -- not supported - https://github.com/neovim/neovim/pull/15723
+            semanticTokens = false,
+            diagnostics = true,
+            schemaRequestService = true,
+          },
+          documentFeatures = {
+            selectionRange = true,
+            foldingRange = true,
+            linkedEditingRange = true,
+            documentSymbol = true,
+            -- not supported - https://github.com/neovim/neovim/pull/13654
+            documentColor = false,
+            documentFormatting = {
+              defaultPrintWidth = 100,
+            },
+          },
+        },
       },
     },
   },
