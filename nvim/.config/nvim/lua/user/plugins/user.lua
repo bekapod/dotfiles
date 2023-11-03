1fb6937783b6c2a0aad13bb0a644a07359e94dc9
Becky <bckyjns@gmail.com>
2023-11-03 20:14:37 +0000

    🎨 catppuccin everywhere

--- nvim/.config/nvim/lua/user/plugins/user.lua
+++ nvim/.config/nvim/lua/user/plugins/user.lua
@@ -10,13 +10,13 @@ return {
   --   end,
   -- },
   {
-    "Mofiqul/dracula.nvim",
-    lazy = false,
-    config = function() end,
-  },
-  {
-    "rose-pine/neovim",
-    lazy = false,
-    config = function() require("rose-pine").setup { variant = "moon" } end,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup {
+        flavor = "macchiato",
+      }
+    end,
   },
 }
