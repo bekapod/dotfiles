6b1b35d9e64b29cc4b612bdc69be43ca8388e538
Becky <bckyjns@gmail.com>
2023-11-08 10:30:43 +0000

    🐛 prevent error when k9s skin already cloned

--- k9s/install.sh
+++ k9s/install.sh
@@ -3,5 +3,9 @@
 set -e
 
 K9S_CONFIG_PATH="${XDG_CONFIG_HOME:-$HOME/Library/Application Support}/k9s"
-git clone https://github.com/catppuccin/k9s.git "${K9S_CONFIG_PATH}/skins/catppuccin" --depth 1
-cp "${K9S_CONFIG_PATH}/skins/catppuccin/dist/macchiato.yml" "${K9S_CONFIG_PATH}/skin.yml"
+if [ -d "${K9S_CONFIG_PATH}/skins/catppuccin" ]; then
+  printf "\n✅ catppuccin skin for k9s is already installed\n"
+else
+  git clone https://github.com/catppuccin/k9s.git "${K9S_CONFIG_PATH}/skins/catppuccin" --depth 1
+  cp "${K9S_CONFIG_PATH}/skins/catppuccin/dist/macchiato.yml" "${K9S_CONFIG_PATH}/skin.yml"
+fi
