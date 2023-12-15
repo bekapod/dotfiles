9b831841d0051a884faeaa7a4b3c30e866869d44
Becky <bckyjns@gmail.com>
2023-12-15 20:15:01 +0000

    💅 formatting

--- nvim/.config/nvim/lua/user/mappings.lua
+++ nvim/.config/nvim/lua/user/mappings.lua
@@ -10,18 +10,20 @@ return {
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
+      desc = "Pick to close",
       function()
         require("astronvim.utils.status").heirline.buffer_picker(
           function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
         )
       end,
-      desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
     -- when applying the next line to the current line, keeps cursor
     -- in the same place instead of moving it to the new end of the line
     ["J"] = { "mzJ`z" },
