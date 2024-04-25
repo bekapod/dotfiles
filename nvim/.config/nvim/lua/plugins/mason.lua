bd23e009a4f21d348e87bfa72e836efc3bece21a
Becky <bckyjns@gmail.com>
2024-04-25 20:38:00 +0100

    🤷♀️ switch to null_ls builtins for formatters?? idk

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -35,11 +35,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "actionlint",
-        "mypy",
-        "prettier",
         "ruff",
-        "stylua",
         -- add more arguments for adding more null-ls sources
       })
     end,
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -5,15 +5,24 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.diagnostics.actionlint,
+      null_ls.builtins.diagnostics.ansiblelint,
+      null_ls.builtins.diagnostics.checkmake,
+      null_ls.builtins.diagnostics.dotenv_linter,
+      null_ls.builtins.diagnostics.markdownlint_cli2,
+      null_ls.builtins.diagnostics.mypy,
+      null_ls.builtins.diagnostics.yamllint,
+      null_ls.builtins.formatting.gleam_format,
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylelint,
+      null_ls.builtins.formatting.stylua,
     }
     return config -- return final config table
   end,
