9b6a3ff007144f5078fdb1cfa2145926a1ebec0c
Becky <becky.jones@prolific.com>
2024-09-05 23:26:11 +0100

    🐛 I dunno, apparently writing the config this way fixed neotest-python

--- nvim/.config/nvim/lua/plugins/test.lua
+++ nvim/.config/nvim/lua/plugins/test.lua
@@ -5,23 +5,26 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "marilari88/neotest-vitest",
+      "nvim-neotest/neotest-python",
     },
-    opts = {
-      adapters = {
-        ["neotest-vitest"] = {},
-        ["neotest-python"] = {
-          is_test_file = function(file_path)
-            if not vim.endswith(file_path, ".py") then
-              return false
-            end
-            local elems = vim.split(file_path, Path.path.sep)
-            local file_name = elems[#elems]
-            return vim.startswith(file_name, "test_")
-              or vim.endswith(file_name, "_test.py")
-              or vim.endswith(file_name, "_tests.py")
-          end,
+    config = function()
+      local neotest = require("neotest")
+
+      neotest.setup({
+        adapters = {
+          require("neotest-vitest"),
+          require("neotest-python")({
+            is_test_file = function(file_path)
+              if not vim.endswith(file_path, ".py") then
+                return false
+              end
+              local elems = vim.split(file_path, Path.path.sep)
+              local file_name = elems[#elems]
+              return vim.endswith(file_name, "_test.py") or vim.endswith(file_name, "_tests.py")
+            end,
+          }),
         },
-      },
-    },
+      })
+    end,
   },
 }
