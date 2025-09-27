48dc3418b4726748e5f1cfad910842e42ba74097
Becky <bckyjns@gmail.com>
2025-09-27 09:38:15 +0100

    🗑️ remove unused blink providers

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -3,44 +3,12 @@ return {
   event = 'VimEnter',
   version = '1.*',
   dependencies = {
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
-      },
-      opts = {},
-    },
-    'folke/lazydev.nvim',
     'giuxtaposition/blink-cmp-copilot',
   },
   --- @module 'blink.cmp'
   --- @type blink.cmp.Config
   opts = {
     keymap = {
-      -- 'default' (recommended) for mappings similar to built-in completions
-      --   <c-y> to accept ([y]es) the completion.
-      --    This will auto-import if your LSP supports it.
-      --    This will expand snippets if the LSP sent a snippet.
-      -- <tab>/<s-tab>: move to right/left of your snippet expansion
-      -- <c-space>: Open menu or open docs if already open
-      -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-      -- <c-e>: Hide menu
-      -- <c-k>: Toggle signature help
       preset = 'default',
     },
 
@@ -49,20 +17,26 @@ return {
     },
 
     completion = {
-      -- By default, you may press `<c-space>` to show the documentation.
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
     },
 
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev', 'copilot', 'codecompanion' },
+      default = { 'lsp', 'path', 'copilot', 'codecompanion' },
       providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         copilot = {
           name = 'copilot',
           module = 'blink-cmp-copilot',
-          kind = 'Copilot',
           score_offset = 100,
           async = true,
+          transform_items = function(_, items)
+            local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = 'Copilot'
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
         },
         codecompanion = {
           name = 'CodeCompanion',
@@ -71,8 +45,6 @@ return {
         },
       },
     },
-
-    snippets = { preset = 'luasnip' },
 
     fuzzy = { implementation = 'lua' },
 
