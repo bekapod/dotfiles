f7fb44c59a579f3ccfa4afac6e291975bd150d9b
Becky <becky@bekapod.dev>
2026-06-26 22:16:44 +0100

    ✨ auto-install treesitter parsers on first encounter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -17,22 +17,35 @@ return {
 
     require('nvim-treesitter').install(parsers)
 
-    local filetypes = {
-      'bash', 'c', 'css', 'diff', 'dockerfile', 'eelixir', 'elixir',
-      'gitconfig', 'gitrebase', 'gitattributes', 'gitcommit', 'gitignore',
-      'gleam', 'go', 'gomod', 'gosum', 'gowork', 'heex', 'html', 'htmldjango',
-      'javascript', 'json', 'lua', 'markdown', 'mermaid', 'ninja', 'php', 'blade', 'python',
-      'query', 'rust', 'scss', 'svelte', 'toml', 'typescriptreact',
-      'typescript', 'vim', 'help', 'vue', 'yaml',
-    }
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = filetypes,
-      callback = function()
-        vim.treesitter.start()
-        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        vim.wo[0][0].foldmethod = 'expr'
+    local function attach(buf, language)
+      if not vim.treesitter.language.add(language) then
+        return
+      end
+      vim.treesitter.start(buf, language)
+      vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo[0][0].foldmethod = 'expr'
+      if vim.treesitter.query.get(language, 'indents') ~= nil then
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end
+    end
+
+    local available = require('nvim-treesitter').get_available()
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        local buf, filetype = args.buf, args.match
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then
+          return
+        end
+
+        local installed = require('nvim-treesitter').get_installed 'parsers'
+        if vim.tbl_contains(installed, language) then
+          attach(buf, language)
+        elseif vim.tbl_contains(available, language) then
+          require('nvim-treesitter').install(language):await(function()
+            attach(buf, language)
+          end)
+        end
       end,
     })
   end,
