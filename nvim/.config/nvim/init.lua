4805eb5eab0de95d26eb5f68d9585bb2db4124f6
Becky <bckyjns@gmail.com>
2026-06-27 09:38:38 +0100

    ✨ add <leader>u to update vim.pack plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,6 +32,7 @@ vim.opt.expandtab = true
 -- [[ Keymaps ]]
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>u', function() vim.pack.update() end, { desc = '[U]pdate plugins' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
