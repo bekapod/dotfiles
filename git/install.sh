b53da5d19bf33420dcf33636fd683cfca4886352
Becky <becky.jones@prolific.com>
2026-09-01 09:04:25 +0100

    ✨ add gh-dash

---
+++ git/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+set -e
+
+echo "💨 Installing GitHub CLI extensions"
+gh extension install dlvhdr/gh-dash
--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ export DOTFILES_LOCATION;
 
 ./bin/dotfiles install oh-my-zsh
 ./bin/dotfiles install brew
+./bin/dotfiles install git
 ./bin/dotfiles install tmux
 ./bin/dotfiles install volta
 ./bin/dotfiles install uv
